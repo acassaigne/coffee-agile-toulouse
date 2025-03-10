ad346d18a9c2c0536a05cf042b5995434ccf0c08
acassaigne <anthony.cassaigne@gmail.com>
2025-03-10 18:13:02 +0100

    feat: Thruthy & falsy

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -7,8 +7,18 @@ class Foo:
     def a_method(self) -> int:
         return 4
 
-    def a_method_b(self, n : int) -> int:
+    def a_method_b(self, n: int) -> int:
         return 4 + n
+
+    def a_method_c(self, p: int, q: int) -> int:
+        return p + q
+
+    def a_mehod_with_union(self, p: bool) -> int | str :
+        if p:
+            return 4
+        return "Hello"
+
+
 
 def my_function(this_a_callable: Callable, p: int) -> int:
     return 10 + this_a_callable(p)
@@ -29,8 +39,16 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(4 + 2, f.a_method_b(2))
 
     def test__a_function_taking_a_method(self):
-
         f = Foo()
         result = my_function(this_a_callable=f.a_method_b, p=3)
 
         self.assertEqual(17, result)
+
+    def test__thruty_falsy(self):
+        self.assertTrue(1)
+        self.assertFalse(0)
+        self.assertFalse(None)
+        self.assertFalse([])
+        self.assertTrue(["one element"])
+
+
